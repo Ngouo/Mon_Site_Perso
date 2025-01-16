866b8969322e7b672ce8e135ffade693b83eb43d
Gilbert <www.ngouogilbert@gmail.com>
2025-01-16 12:48:46 +0000

    config du deploiement

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-MEDIA_URL = 'Images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Images/')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
